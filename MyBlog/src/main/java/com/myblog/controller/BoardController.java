9744978a8a75055494993f0a09c3b2a46d9def45
ohsky97 <44282306+ohsky97@users.noreply.github.com>
2020-09-16 20:51:02 +0900

    Update BoardController.java

--- MyBlog/src/main/java/com/myblog/controller/BoardController.java
+++ MyBlog/src/main/java/com/myblog/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.myblog.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
